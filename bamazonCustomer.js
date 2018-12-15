86edde244cf50c01d66b0a31ef500b54afdfc1b1
ByronRoberts <byron.p.roberts@gmail.com>
2018-12-15 09:57:02 -0600

    Added Screenshots and fixed out of stock bug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,18 +20,13 @@ var connection = mysql.createConnection({
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
-    // runPrompt1();
-    // connection.end();
   });
   function readProducts() {
       console.log("Welcome to Bamazon! Here are our products.")
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
-  
-      // Log all results of the SELECT statement
       console.table(res);
       runPrompt1();
-    //   connection.end();
     function runPrompt1(){
         inquirer
         .prompt([{
@@ -53,12 +48,13 @@ var connection = mysql.createConnection({
             }
             }])
         .then(function(answer){
-            console.log(answer.item);
+            // console.log(answer.item);
             console.log(answer.amount);
-            // console.log(answer.item + " Item")
             var query = "SELECT * FROM Products WHERE item_id =" + answer.item;
             connection.query(query, function(err, res){
-                if(answer.amount <= res) {
+                for(var i = 0; i< res.length; i++){
+                // console.log(res)
+                if(answer.amount <= res[i].stock_quantity) {
                     for(var i = 0; i< res.length; i++){
                         console.log("We currently have " + res[i].stock_quantity + " of the Item you requested.");
                         console.log("Your order of "+ answer.amount +" " + res[i].product_name + " is being processed!");
@@ -69,32 +65,8 @@ var connection = mysql.createConnection({
                         console.log("Sorry we are out of stock on that Item.");
                     }
                     connection.end();
-            })
+            }})
         })
         }
     });
   }
-//   function runPrompt1(){
-//     inquirer
-//     .prompt([{
-//         name:"Item #",
-//         type:"rawlist",
-//         message:"What item # would you like to buy?",
-//         choices:["1","2","3","4","5","6","7","8","9","10"],
-//         },
-//         {
-//         name:"amount",
-//         type:"input",
-//         message: "How many would you like to purchase?"
-//         }
-        
-//         // choices:[res.item_id.length]
-
-//     ])
-//     .then(function(answer){
-//         console.log(answer)
-        
-//     })
-    
-//     }
-  
